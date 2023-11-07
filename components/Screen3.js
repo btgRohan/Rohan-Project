adf1ec0b9bf8f57dafbfc10a2b7f87c4be6042aa
btgRohan <rohan@hostingproviderindia.com>
2023-11-07 20:31:36 +0530

    Changes in Screen3 Components

--- components/Screen3.js
+++ components/Screen3.js
@@ -1,39 +1,72 @@
 import React from "react";
-import { Button, Actionsheet, useDisclose, Text, Box, Center, NativeBaseProvider } from "native-base";
+import {
+  Button,
+  Actionsheet,
+  useDisclose,
+  Text,
+  Box,
+  Center,
+  NativeBaseProvider,
+} from "native-base";
 
 function Example() {
-  const {
-    isOpen,
-    onOpen,
-    onClose
-  } = useDisclose();
-  return <Center>
-      <Button onPress={onOpen}>Actionsheet</Button>
+  const { isOpen, onOpen, onClose } = useDisclose();
+  return (
+    <Center>
+      <Button
+        onPress={onOpen}
+        colorScheme="teal" // Change the button color
+        size="lg" // Adjust button size
+        borderRadius={8} // Add border radius
+        _text={{ color: "white" }} // Set text color
+      >
+        Open Actionsheet
+      </Button>
       <Actionsheet isOpen={isOpen} onClose={onClose}>
         <Actionsheet.Content>
-          <Box w="100%" h={60} px={4} justifyContent="center">
-            <Text fontSize="16" color="gray.500" _dark={{
-            color: "gray.300"
-          }}>
+          <Box
+            w="100%"
+            h={60}
+            px={4}
+            justifyContent="center"
+            bg="teal.500" // Change the background color
+            _dark={{
+              bg: "teal.700",
+            }}
+          >
+            <Text
+              fontSize="20" // Increase font size
+              color="white" // Change text color
+              _dark={{
+                color: "teal.200",
+              }}
+            >
               Albums
             </Text>
           </Box>
+          <Actionsheet.Item>Choose an Action:</Actionsheet.Item>
           <Actionsheet.Item>Delete</Actionsheet.Item>
           <Actionsheet.Item isDisabled>Share</Actionsheet.Item>
           <Actionsheet.Item>Play</Actionsheet.Item>
           <Actionsheet.Item>Favourite</Actionsheet.Item>
-          <Actionsheet.Item>Cancel</Actionsheet.Item>
+          <Actionsheet.Item
+            onPress={onClose} // Close the Actionsheet
+            _text={{ color: "teal.500" }} // Set text color
+          >
+            Cancel
+          </Actionsheet.Item>
         </Actionsheet.Content>
       </Actionsheet>
-    </Center>;
+    </Center>
+  );
 }
 
-    export default () => {
-        return (
-          <NativeBaseProvider>
-            <Center flex={1} px="3">
-                <Example />
-            </Center>
-          </NativeBaseProvider>
-        );
-    };
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Example />
+      </Center>
+    </NativeBaseProvider>
+  );
+};
